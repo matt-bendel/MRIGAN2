45bd4f54732b6868a3144ce55e66053329798a8e
Matt Bendel <bendel.8@osu.edu>
2021-10-20 00:04:58 -0400

    many debugging commits incoming

--- utils/parse_args.py
+++ utils/parse_args.py
@@ -1,6 +1,42 @@
 import pathlib
+import argparse
 
-from utils.args import Args
+
+class Args(argparse.ArgumentParser):
+    """
+    Defines global default arguments.
+    """
+
+    def __init__(self, **overrides):
+        """
+        Args:
+            **overrides (dict, optional): Keyword arguments used to override default argument values
+        """
+
+        super().__init__(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+        self.add_argument('--seed', default=42, type=int, help='Seed for random number generators')
+        self.add_argument('--resolution', default=320, type=int, help='Resolution of images')
+
+        # Data parameters
+        self.add_argument('--challenge', choices=['singlecoil', 'multicoil'], default='singlecoil',
+                          help='Which challenge')
+        self.add_argument('--data-path', type=pathlib.Path, required=True,
+                          help='Path to the dataset')
+        self.add_argument('--sample-rate', type=float, default=1.,
+                          help='Fraction of total volumes to include')
+
+        # Mask parameters
+        self.add_argument('--accelerations', nargs='+', default=[4], type=int,
+                          help='Ratio of k-space columns to be sampled. If multiple values are '
+                               'provided, then one of those is chosen uniformly at random for '
+                               'each volume.')
+        self.add_argument('--center-fractions', nargs='+', default=[0.08], type=float,
+                          help='Fraction of low-frequency k-space columns to be sampled. Should '
+                               'have the same length as accelerations')
+
+        # Override defaults with passed overrides
+        self.set_defaults(**overrides)
 
 
 def create_arg_parser():
@@ -45,6 +81,5 @@ def create_arg_parser():
                         help='Path to an existing discriminator checkpoint. Used along with "--resume"')
     parser.add_argument('--checkpoint_unet', type=str,
                         help='Path to an existing unet checkpoint. Used along with "--resume"')
-
 
     return parser
